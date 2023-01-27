1c1e05db5d01f3afc360308dce2ee275a7f33d77
Lubembe Michael <lubemb@gmail.com>
2023-01-27 12:18:42 +0300

    fixed betty style coding

--- get_print.c
+++ get_print.c
@@ -2,44 +2,38 @@
 
 /**
  * get_print - selects printing func
- * struct - gets
  * depending on the conversion specifier passed to _printf
- * character that holds the conversion specifier
- * @format: ...
+ * @s: character that holds the conversion specifier
  * Description: the function loops through the structs array
  * func_arr[] to find a match between the specifier passed to _printf
  * and the first element of the struct, and then the approriate
  * printing function
  * Return: a pointer to the matching printing function
-*/
+ */
+int (*get_print(char s))(va_list, flags_t *)
+{
+	ph func_arr[] = {
+		{'i', print_int},
+		{'s', print_string},
+		{'c', print_char},
+		{'d', print_int},
+		{'u', print_unsigned},
+		{'x', print_hex},
+		{'X', print_hex_big},
+		{'b', print_binary},
+		{'o', print_octal},
+		{'R', print_rot13},
+		{'r', print_rev},
+		{'S', print_bigS},
+		{'p', print_address},
+		{'%', print_percent}
+		};
+	int flags = 14;
 
-int (*get_print(char format))(va_list, flags_t *)
-{
-struct
-{
-	char format;
-	int (*f)(va_list, flags_t *);
-	} func_arr[] = {
-	{'i', print_int},
-	{'s', print_string},
-	{'c', print_char},
-	{'d', print_int},
-	{'u', print_unsigned},
-	{'x', print_hex},
-	{'X', print_hex_big},
-	{'b', print_binary},
-	{'o', print_octal},
-	{'R', print_rot13},
-	{'r', print_rev},
-	{'S', print_bigS},
-	{'p', print_address},
-	{'%', print_percent}
-};
+	register int i;
 
-	for (size_t i = 0; i < sizeof(func_arr) / sizeof(func_arr[0]); i++)
-{
-	if (func_arr[i].format == format)
-		return (func_arr[i].f);
-	}
+	for (i = 0; i < flags; i++)
+		if (func_arr[i].c == s)
+			return (func_arr[i].f);
 	return (NULL);
 }
