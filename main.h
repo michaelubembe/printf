55049fcb7b5b8525da5eaf06e650052eb4c49bbf
Lubembe Michael <lubemb@gmail.com>
2023-01-27 03:01:47 +0300

    modified descriptions

--- main.h
+++ main.h
@@ -8,7 +8,7 @@
  * struct flags - struct containing flags to "turn on"
  * when a flag specifier is passed to _printf()
  * @plus: flag for the '+' character
- * @space: flag for the ' ' character
+ * @space: flag for the ' '
  * @hash: flag for the '#' character
  */
 typedef struct flags
